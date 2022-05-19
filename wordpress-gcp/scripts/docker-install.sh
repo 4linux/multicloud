0514ff3820666e597944e5e68938c686827b61ae
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2022-05-19 19:26:35 -0300

    Update docker-install.sh

--- wordpress-gcp/scripts/docker-install.sh
+++ wordpress-gcp/scripts/docker-install.sh
@@ -8,8 +8,8 @@ sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 
 # Instala o Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/sbin/docker-compose
-sudo chmod +x /usr/local/sbin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
+sudo chmod +x /bin/docker-compose
 
 # Cria a rede moodle-net no Docker
 sudo docker network create moodle-net

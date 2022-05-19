0ed8dd763efff6073b180dc07c7dc1a63433bf55
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2022-05-19 19:26:15 -0300

    Update dbserver-webserver-install.sh

--- wordpress-gcp/userdata/dbserver-webserver-install.sh
+++ wordpress-gcp/userdata/dbserver-webserver-install.sh
@@ -9,8 +9,8 @@ apt update
 apt install docker-ce docker-ce-cli containerd.io git vim -y
 
 # Instala o Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/sbin/docker-compose
-chmod +x /usr/local/sbin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
+chmod +x /bin/docker-compose
 
 # Cria a rede wp-net no Docker
 docker network create wp-net

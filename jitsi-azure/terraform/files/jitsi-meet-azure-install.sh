de11dcf0ba549468b2c5e3706141c789d097254d
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2022-05-22 20:08:33 -0300

    Update and rename jitsi-jvb-azure-install.sh to jitsi-meet-azure-install.sh

---
+++ jitsi-azure/terraform/files/jitsi-meet-azure-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Remove as configurações de Log do arquivo docker-compose.yml
+sed -i '/^[[:blank:]]*logging/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-meet-jvb/docker-compose.yml 
+sed -i '/^[[:blank:]]*driver/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-meet-jvb/docker-compose.yml
+sed -i '/^[[:blank:]]*options/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-meet-jvb/docker-compose.yml
+sed -i '/^[[:blank:]]*syslog/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-meet-jvb/docker-compose.yml
+sed -i '/^[[:blank:]]*tag/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-meet-jvb/docker-compose.yml
+
+# Atualiza o container jitsi-meet
+cd /home/azureuser/multicloud/jitsi-azure/compose-meet-jvb/
+docker-compose up -d

cda82405191b92e84f1ea8b7fda4aa08819c823d
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2022-05-22 20:09:37 -0300

    Update variables.tf

--- jitsi-azure/terraform/variables.tf
+++ jitsi-azure/terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "location" {
   description = "Define em qual regiao a instancia sera executada"
-  default = "brazilsouth"
+  default = "southeastasia"
 }
 
 variable "resource_group_name" {
   description = "Define o nome do grupo de recurso"
-  default = "jitsi"
+  default = "multicloud"
 }
 
 variable "name_vm" {
@@ -13,9 +13,14 @@ variable "name_vm" {
   default = "jitsi-meet-azure-terraform"
 }
 
+variable "image_id" {
+  description = "Define o id da imagem modelo"
+  default = "/subscriptions/0009f13e-68f4-4cae-8cb2-9e26239157a1/resourceGroups/multicloud/providers/Microsoft.Compute/images/imagem-modelo-jitsi-meet"
+}
+
 variable "subnet_id" {
   description = "Define o nome da subrede"
-  default = "/subscriptions/51e3f409-d738-40cc-898b-0cff5cdf4142/resourceGroups/jitsi/providers/Microsoft.Network/virtualNetworks/jitsi-network/subnets/default"
+  default = "/subscriptions/0009f13e-68f4-4cae-8cb2-9e26239157a1/resourceGroups/multicloud/providers/Microsoft.Network/virtualNetworks/jitsi-vmnet/subnets/default"
 }
 
 variable "name_ip" {
@@ -35,15 +40,15 @@ variable "name_ip_configuration" {
 
 variable "network_security_group_id_meet" {
   description = "Define o nome do grupo de seguranca jitsi-meet"
-  default = "/subscriptions/51e3f409-d738-40cc-898b-0cff5cdf4142/resourceGroups/jitsi/providers/Microsoft.Network/networkSecurityGroups/jitsi-meet"
+  default = "/subscriptions/0009f13e-68f4-4cae-8cb2-9e26239157a1/resourceGroups/multicloud/providers/Microsoft.Network/networkSecurityGroups/jitsi-meet"
 }
 
 variable "size" {
   description = "Define o tipo de instancia"
-  default = "Standard_DS1_v2"
+  default = "Standard_DC1ds_v3"
 }
 
 variable "user" {
   description = "Define o nome do usuario que tera acesso SSH na instancia"
-  default = "suporte-azure"
+  default = "azureuser"
 }

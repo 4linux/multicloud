33fc536d7eebf97af07f018b3a07c7daa503641a
4Linux <67426067+curso-4linux@users.noreply.github.com>
2023-10-23 10:11:53 -0300

    Update awslogs-install.sh

--- moodle-aws/scripts/awslogs-install.sh
+++ moodle-aws/scripts/awslogs-install.sh
@@ -6,8 +6,9 @@
 yum install amazon-cloudwatch-agent rsyslog -y
 
 ### Configura o arquivo amazon-cloudwatch-agent.json
-wget https://raw.githubusercontent.com/4linux/multicloud/main/moodle-aws/scripts/amazon-cloudwatch-agent.json -O /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
-amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+mkdir /opt/aws/amazon-cloudwatch-agent/etc/custom
+wget https://raw.githubusercontent.com/4linux/multicloud/main/moodle-aws/scripts/amazon-cloudwatch-agent.json -O /opt/aws/amazon-cloudwatch-agent/etc/custom/amazon-cloudwatch-agent.json
+amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/custom/amazon-cloudwatch-agent.json
 
 ### Configura os serviços amazon-cloudwatch-agent e rsyslog
 systemctl start amazon-cloudwatch-agent rsyslog
